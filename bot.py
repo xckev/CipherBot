9eb584293cc495cdebe8052741e6f9476a5b6aa4
xckev <xckevin@outlook.com>
2022-07-27 14:11:53 -0700

    first version of vote command

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+from discord.ui import button, View, Button
+from discord.interactions import Interaction
 from discord import app_commands
 import os
 from dotenv import load_dotenv
@@ -34,27 +36,42 @@ class CipherBot(commands.Bot):
         print(bot.user.id)
         print('----------')
 
-'''
-bot = commands.Bot(command_prefix='$', intents=intnts, application_id=996571275919097898)
-
-@bot.event
-async def on_ready():
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game('hard to get'))
-    print('Logged in as:')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('----------')
-
-initial_extensions = []
-for filename in os.listdir('./cogs'):
-    if(filename.endswith('.py')):
-       initial_extensions.append("cogs." + filename[:-3])
-
-
-if __name__ == '__main__':
-    for extension in initial_extensions:
-        bot.load_extension(extension)
-    #print('cogs:', initial_extensions)
-'''
 bot = CipherBot()
+
+class Menu(View):
+    def __init__(self, *, timeout=300):
+        super().__init__(timeout=timeout)
+        self.yescount = 0
+        self.nocount = 0
+        self.voted = []
+        #self.value = None
+
+    @button(label="Yes", style=discord.ButtonStyle.green)
+    async def yes(self, interaction:Interaction, button:Button):
+        if(interaction.user in self.voted):
+            await interaction.response.send_message("You have already voted.", ephemeral=True)
+        else:
+            self.yescount += 1
+            self.voted.append(interaction.user)
+            await interaction.response.send_message("You voted yes", ephemeral=True)
+            await interaction.followup.send(f"Yes: {str(self.yescount)}  |  No: {str(self.nocount)}")
+        #await interaction.response.send_message(f"Yes: {str(self.yescount)}  |  No: {str(self.nocount)}")
+
+    @button(label="No", style=discord.ButtonStyle.red)
+    async def no(self, interaction:Interaction, button:Button):
+        if(interaction.user in self.voted):
+            await interaction.response.send_message("You have already voted.", ephemeral=True)
+        else:
+            self.nocount += 1
+            self.voted.append(interaction.user)
+            await interaction.response.send_message("You voted no", ephemeral=True)
+            await interaction.followup.send(f"Yes: {str(self.yescount)}  |  No: {str(self.nocount)}")
+        #await interaction.response.send_message(f"Yes: {str(self.yescount)}  |  No: {str(self.nocount)}")
+
+@bot.command()
+async def vote(ctx, *, question: str):
+    v = Menu()
+    await ctx.reply(f"Vote: {question}", view=v)
+
+
 bot.run(Token)
